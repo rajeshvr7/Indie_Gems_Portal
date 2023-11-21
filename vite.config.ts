6304481fb7181236c514b596658c3e8bfa8be392
rajeshvr7 <125723807+rajeshvr7@users.noreply.github.com>
2023-11-21 22:57:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Obtain your IP address
+const ipAddress = '172.31.45.9';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Specify the host (IP address) and port
+    host: ipAddress,
+    port: 5173, // You can change this port if needed
+  },
+});
